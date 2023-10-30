2afe79879e64782a9af2e9f11bfa115dea3d10ea
IljaNoskov <99073996+IljaNoskov@users.noreply.github.com>
2023-10-30 23:27:57 +0300

    Update server.py

--- task2_json_valid/server.py
+++ task2_json_valid/server.py
@@ -22,10 +22,8 @@ HOST = "127.0.0.1"
 PORT = 65432
 
 # Запускаю сервер до передачи b'close'
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((HOST, PORT))
-data = ''
-while data != b'close':
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, PORT))
     s.listen()
     conn, addr = s.accept()
     with conn:
