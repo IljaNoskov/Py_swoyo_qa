15797a263028c7b5ef4d861f1d3659e803b54568
IljaNoskov <ilja.noskovv@gmail.com>
2023-11-27 17:17:06 +0300

    fix second task after feedback

--- task2_json_valid/client.py
+++ task2_json_valid/client.py
@@ -1,6 +1,6 @@
 import socket
 
-json_mas = [[b"""{"menu": {"id": "file",
+json_mas = [["""{"menu": {"id": "file",
                            "value": "File",
                            "popup": {"menuitem": [{"value": "New", "onclick": "CreateNewDoc()"},
                                                   {"value": "Open", "onclick": "OpenDoc()"},
@@ -8,8 +8,8 @@ json_mas = [[b"""{"menu": {"id": "file",
                                                   ]
                                      }
                            }
-                }""", b'True'],
-            [b"""{"widget": {"debug": "on",
+                }""", 'True'],
+            ["""{"widget": {"debug": "on",
                              "window": {"title": "Sample Konfabulator Widget",
                              "name": "main_window",
                              "width": 500,
@@ -31,9 +31,9 @@ json_mas = [[b"""{"menu": {"id": "file",
                            "onMouseUp": "sun1.opacity = (sun1.opacity / 100) * 90;"
                            }
                   }
-            }""", b'True'],
-            [b"""Not a json""", b'False'],
-            [{1: "one", 2: "two"}, b'False']]
+            }""", 'True'],
+            ["""Not a json""", 'False'],
+            ['{1: "one", 2: "two"}', 'False']]
 
 HOST = "127.0.0.1"
 PORT = 65432
@@ -42,7 +42,8 @@ PORT = 65432
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
     res = input('Send your data(1) or my data(0): ')
-    if res == '1':
+    print(res)
+    if res == b'1':
         send_json = input('Input your json:')
         s.sendall(bytes(send_json, 'utf-8'))
         data = s.recv(1024)
@@ -50,10 +51,10 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     else:
         # Проверяю в цикле, что все результаты соответствуют ожидаемым
         for json, ans in json_mas:
-            s.sendall(bytes(json))
-            data = s.recv(1024)
+            s.sendall(bytes(json, 'utf-8'))
+            data = s.recv(1024).decode("utf-8")
             assert data == ans, f"{json} \nMy json valid is broken, my ans is {ans}, real ans is {str(data)}"
-            print(f'{json} is real json? Answer: {data}', '\n')
+            print(f'{json} \nis real json? Answer: {data}', '\n')
         print("All tests are success")
     # отправляю close чтобы закрыть соединение
     s.sendall(b"close")
--- task2_json_valid/server.py
+++ task2_json_valid/server.py
@@ -5,15 +5,13 @@ import json
 def is_json(my_json):
     try:
         # Проверяю на возможность перевести из json
-        json.loads(my_json)
+        print(my_json)
+        my_json = json.loads(my_json.decode("utf-8"))
+        if type(my_json) == 'dict':
+            return b'False'
+        return b'True'
     except ValueError as e:
         return b'False'
-    try:
-        # Проверяю, что это не число (например '123')
-        int(my_json)
-    except ValueError as e:
-        return b'True'
-    return b'False'
 
 
 print('start server')
@@ -31,7 +29,7 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         while True:
             data = conn.recv(1024)
             # проверяю что сообщение не пустое и не close
-            if data and data != b'close':
+            if data != b'close':
                 # Отправляю результат работы функции на переданных данных
                 conn.sendall(is_json(data))
             else:
