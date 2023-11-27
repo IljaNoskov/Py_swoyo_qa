b595a701b09ef06d5880db939a1e4d97ef9ef1cc
IljaNoskov <ilja.noskovv@gmail.com>
2023-11-27 23:10:28 +0300

    fix first task (no comments)

--- task1_Api/booking.py
+++ task1_Api/booking.py
@@ -4,9 +4,9 @@ import json
 
 # Класс для бронирования - каждый метод соответствующий вопрос
 class Booking:
-    def __init__(self, id='-1', firstname='Jim', lastname='Brown', total_price=111, deposit_paid='true',
+    def __init__(self, booking_id='-1', firstname='Jim', lastname='Brown', total_price=111, deposit_paid='true',
                  chekin='2018-01-01', chekout='2019-01-01', additional_needs='Breakfast'):
-        self.id = id
+        self.booking_id = booking_id
         self.firstname = firstname
         self.lastname = lastname
         self.total_price = total_price
@@ -28,37 +28,57 @@ class Booking:
     def create(self):
         result = requests.post('https://restful-booker.herokuapp.com/booking',
                                headers={'Content-Type': 'application/json'}, data=self.make_json())
-        self.id = result.json()['bookingid']
+        self.booking_id = result.json()['bookingid']
+        if result.status_code != 200:
+            return 'error with post'
         return result.content
 
     # Обновляю даты бронирование через полное изменение (см сайт)
     def update_date(self, new_chekin, new_chekout):
-        if self.id == -1:
+        if self.booking_id == -1:
             result = 'Вы ещё не забронировали номер или уже удалили бронь'
         else:
             self.chekin = new_chekin
             self.chekout = new_chekout
-            result = requests.put('https://restful-booker.herokuapp.com/booking/' + str(self.id),
+            result = requests.put('https://restful-booker.herokuapp.com/booking/' + str(self.booking_id),
                                   headers={'Content-Type': 'application/json', 'Accept': 'application/json',
                                           'Authorization': 'Basic YWRtaW46cGFzc3dvcmQxMjM='}, data=self.make_json())
+        if result.status_code != 200:
+            return 'error with put'
         return result.content
 
     # Беру информацию с сайта
     def get(self):
-        if self.id == -1:
+        if self.booking_id == -1:
             result = 'Вы ещё не забронировали номер или уже удалили бронь'
         else:
-            result = requests.get('https://restful-booker.herokuapp.com/booking/' + str(self.id)).content
-        return result
+            result = requests.get('https://restful-booker.herokuapp.com/booking/' + str(self.booking_id))
+            if result.status_code != 200:
+                return 'error with get'
+        return result.content
 
     # Удаляю бронирование с сайта
     def delete(self):
-        if self.id == -1:
+        if self.booking_id == -1:
             result = 'Вы ещё не забронировали номер или уже удалили бронь'
         else:
-            result = requests.delete('https://restful-booker.herokuapp.com/booking/' + str(self.id),
+            result = requests.delete('https://restful-booker.herokuapp.com/booking/' + str(self.booking_id),
                                      headers={'Content-Type': 'application/json',
                                               'Authorization': 'Basic YWRtaW46cGFzc3dvcmQxMjM='})
-            # Не меняю id на -1 чтобы после использования get возвращалось "Not found" (значит запись удалена)
-            # self.id = -1
+            if result.status_code != 201:
+                return 'error with delete'
+            elif self.get() != 'error with get':
+                return 'error: find booking after delete'
+            else:
+                self.id = -1
         return result
+
+
+'''
+print('new booking')
+k = Booking()
+print(k.create())
+print(k.get())
+print(k.update_date('2022-01-01', '2023-01-01'))
+print(k.delete())
+'''
--- task1_Api/main.py
+++ task1_Api/main.py
@@ -1,55 +1,77 @@
-import Booking
+from booking import Booking
 
 
-def work_with_booking(booking):
-    # О вариативности действий
-    print('Select the action you want with the reservation')
-    print('1 - create a new booking')
-    print('2 - get information about an already created reservation')
-    print('3 - change booking dates')
-    print('4 - delete a reservation')
-    print('5 - terminate the program')
-    num = input()
-    # в зависимости от цифры отправляю разные запросы
-    if num == '1':
-        print('1 - Create a reservation with standard data')
-        print('2 - Create a reservation with your own data')
+def work_with_booking_list(b_list):
+    def work_with_booking(booking):
+        # О вариативности действий
+        print('Select the action you want with the reservation')
+        print('1 - create a new booking')
+        print('2 - get information about an already created reservation')
+        print('3 - change booking dates')
+        print('4 - delete a reservation')
+        print('5 - terminate the program')
         num = input()
-        if num == '2':
-            my_firstname = input('Enter your first name')
-            my_lastname = input('Enter your last name')
-            my_total_price = input('Enter the price per room')
-            my_deposit_paid = input('Enter the availability of the deposit in the format true or false')
-            my_chekin = input('select your chekin date in format "YYYY-MM-DD": ')
-            my_chekout = input('select your chekout date in format "YYYY-MM-DD": ')
-            my_additional_needs = input('Enter your other needs: ')
-            booking = Booking.Booking(-1, my_firstname, my_lastname, my_total_price, my_deposit_paid, my_chekin,
-                                      my_chekout, my_additional_needs)
-        elif num != '1':
+        print(num, num == '5')
+        # в зависимости от цифры отправляю разные запросы
+        if num == '1':
+            print('1 - Create a reservation with standard data')
+            print('2 - Create a reservation with your own data')
+            num = input()
+            if num == '2':
+                my_firstname = input('Enter your first name')
+                my_lastname = input('Enter your last name')
+                my_total_price = input('Enter the price per room')
+                my_deposit_paid = input('Enter the availability of the deposit in the format true or false')
+                my_chekin = input('select your chekin date in format "YYYY-MM-DD": ')
+                my_chekout = input('select your chekout date in format "YYYY-MM-DD": ')
+                my_additional_needs = input('Enter your other needs: ')
+                booking = Booking(-1, my_firstname, my_lastname, my_total_price, my_deposit_paid, my_chekin,
+                                          my_chekout, my_additional_needs)
+            elif num != '1':
+                print('Incorrect input')
+            print('Reservation creation report')
+            print(booking.create())
+        elif num == '2':
+            print('Your booking report')
+            print(booking.get())
+        elif num == '3':
+            chekin = input('select your chekin date in format "YYYY-MM-DD": ')
+            chekout = input('select your chekout date in format "YYYY-MM-DD": ')
+            print('Date update eeport')
+            print(booking.update_date(chekin, chekout))
+        elif num == '4':
+            print('Delete report')
+            print(booking.delete())
+        elif num == '5':
+            print('stop')
+            return 0
+        else:
             print('Incorrect input')
-        print('Reservation creation report')
-        print(booking.create())
-    elif num == '2':
-        print('Your booking report')
-        print(booking.get())
-    elif num == '3':
-        chekin = input('select your chekin date in format "YYYY-MM-DD": ')
-        chekout = input('select your chekout date in format "YYYY-MM-DD": ')
-        print('Date update eeport')
-        print(booking.update_date(chekin, chekout))
-    elif num == '4':
-        print('Delete report')
-        print(booking.delete())
-    elif num == '5':
-        return 0
+        print()
+
+    if len(b_list) > 0:
+        for i in range(len(b_list)):
+            print(f'booking № {i}: {b_list[i].make_json()}')
+            print()
+            n = int(input('Chose your booking number (or -1 to stop:'))
+            if n == -1:
+                return 0
+            while True:
+                if work_with_booking(b_list[n]) == 0:
+                    break
     else:
-        print('Incorrect input')
-    print()
+        b = Booking()
+        b_list.append(b)
+        print('start work solo booking')
+        while True:
+            if work_with_booking(b) == 0:
+                break
 
 
 if __name__ == '__main__':
     # Запускаю в вечном цикле работу с бронированием
-    my_booking = Booking.Booking()
+    booking_list = []
     while True:
-        if work_with_booking(my_booking) == 0:
+        print('start work with list')
+        if work_with_booking_list(booking_list) == 0:
             break
